7d2ed0284b6d35150c6a980077d4a14d097a3d1d
MrGafoor <145122124+MrGafoor@users.noreply.github.com>
2025-02-26 15:35:34 +0530

    Add files via upload

---
+++ StudentGrades.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class StudentGrades{
+	public static void main (String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println();
+		System.out.print("Enter you English subject marks : ");
+		int Eng=sc.nextInt();
+		System.out.println();
+		System.out.print("Enter you Hindi subject marks : ");
+		int Hin=sc.nextInt();
+		System.out.println();
+		System.out.print("Enter you Mathametics subject marks : ");
+		int Math=sc.nextInt();
+		System.out.println();
+		System.out.print("Enter you Physics subject marks : ");
+		int Phy=sc.nextInt();
+		System.out.println();
+		System.out.print("Enter you Biology subject marks : ");
+		int Bio=sc.nextInt();
+		System.out.println();
+		System.out.print("Enter you Social subject marks : ");
+		int Social=sc.nextInt();
+		System.out.println();
+		float totalSubjects=6;
+		
+		float TotalMarks=Eng+Hin+Math+Phy+Bio+Social; //Total marks
+		System.out.println("==>Your results are:");
+		System.out.println();
+		System.out.println("--- Your total marks are : "+ TotalMarks+" ---");
+
+		float AverageValue=AverageMarks(TotalMarks,totalSubjects);
+
+		Grades(AverageValue);
+		
+		
+	}
+	public static float AverageMarks(float TotalMarks,float totalSubjects){
+		float Average=TotalMarks/(totalSubjects);
+		System.out.println();
+		System.out.println("--- The Average marks are: "+Average+" ---");
+	return Average;
+	}
+
+
+	public static void Grades(float AverageValue){
+		System.out.println();
+		if(AverageValue>=90){
+			System.out.println("--- You Secured 'S' Grade ---");
+		}else if(AverageValue<90&& AverageValue>=80){
+			System.out.println("--- You Secured 'A+' Grade ---");
+		}else if(AverageValue<80&& AverageValue>=70){
+			System.out.println("--- You Secured 'A' Grade ---");
+		}else if(AverageValue<70&& AverageValue>=60){
+			System.out.println("--- You Secured 'B+' Grade ---");
+		}else if(AverageValue<60&& AverageValue>=50){
+			System.out.println("--- You Secured 'B' Grade ---");
+		}else if(AverageValue<50&& AverageValue>=40){
+			System.out.println("--- You Secured 'C' Grade ---");
+		}else{
+			System.out.println("--- You Secured 'F' Grade, and You are failed to promote for next year..! ---");
+		}
+	}
+}
